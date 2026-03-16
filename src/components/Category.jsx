1777a4c4fc834f742816e3cff5228281def503c4
mhself <mhmmiripoor@gmail.com>
2026-03-16 20:54:07 +0330

    Category Personalized

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,10 +3,7 @@ import catImg1 from "../assets/category-1.png";
 import catImg2 from "../assets/category-2.png";
 import catImg3 from "../assets/category-3.png";
 import catImg4 from "../assets/category-4.png";
-import ctImg1 from "../assets/ct1.png";
-import ctImg2 from "../assets/ct2.png";
-import ctImg3 from "../assets/ct3.png";
-import ctImg4 from "../assets/ct4.png";
+
 
 const Category = () => {
     return (
@@ -32,9 +29,10 @@ const Category = () => {
                     <img className='w-[270px] h-[288px] object-contain md:w-full md:max-w-[270px] border-0' src={catImg3} />
                 </div>
                 <div className='md:flex md:flex-col md:items-center'>
-                    <img className='w-[270px] h-[286px] object-contain md:w-full md:max-w-[270px]' src={catImg4} />
-                    <div className='w-[270px] h-[118px] bg-[#EBEEF3] flex items-center justify-center my-4 md:w-full md:max-w-[270px]'>
-                        <img className='w-[210px] h-[78px] object-contain' src={ctImg4} />
+                    <img className='w-[270px] h-[286px] object-contain md:w-full md:max-w-[270px] rounded-2' src={catImg4} />
+                    <div className='w-[270px] h-[118px] bg-[#EBEEF3] flex items-center justify-center my-4 md:w-full md:max-w-[270px] '>
+                    <span className='font-semibold text-3xl'> تنیس روی میز</span>
+
                     </div>
                 </div>
             </div>
